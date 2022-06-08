ff0e282c7e8fdcc925d6178f844de86617732f38
Gautam Sinha <gautam.sinha@live.com>
2022-06-08 14:06:34 -0400

    created camera component

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello !Max</Text>
+      <Text>Camera</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
